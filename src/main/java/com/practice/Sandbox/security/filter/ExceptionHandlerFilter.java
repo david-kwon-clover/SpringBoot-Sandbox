595ea3b0da2e8f898c9fc54ba30a08398dde75cf
David Kwon <david.kwon@clover.com>
2024-12-19 15:01:04 -0700

    Add ExceptionHandlerFilter

---
+++ src/main/java/com/practice/Sandbox/security/filter/ExceptionHandlerFilter.java
@@ -0,0 +1,22 @@
+package com.practice.Sandbox.security.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class ExceptionHandlerFilter extends OncePerRequestFilter {
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    try {
+      filterChain.doFilter(request, response);
+    } catch (RuntimeException e) {
+      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+      response.getWriter().write("BAD REQUEST");
+      response.getWriter().flush();
+    }
+  }
+}
