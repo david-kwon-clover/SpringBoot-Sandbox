417484fa3c1bb2f314dc64b6e676769b066811e8
David Kwon <david.kwon@clover.com>
2024-12-19 14:15:14 -0700

    Add BCrypt password encoder

--- src/main/java/com/practice/Sandbox/SandboxApplication.java
+++ src/main/java/com/practice/Sandbox/SandboxApplication.java
@@ -2,6 +2,8 @@ package com.practice.Sandbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class SandboxApplication {
@@ -10,4 +12,8 @@ public class SandboxApplication {
 		SpringApplication.run(SandboxApplication.class, args);
 	}
 
+	@Bean
+	BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
--- src/main/java/com/practice/Sandbox/service/UserServiceImpl.java
+++ src/main/java/com/practice/Sandbox/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.practice.Sandbox.service;
 import com.practice.Sandbox.entity.User;
 import com.practice.Sandbox.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,9 +11,11 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
 
   UserRepository userRepository;
+  BCryptPasswordEncoder bCryptPasswordEncoder;
 
   @Override
   public void saveUser(User user) {
+    user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
     userRepository.save(user);
   }
 }
