1bb9c52576c4e292a4cfaf291c6bf527f8bf3f02
David Kwon <david.kwon@clover.com>
2024-12-19 15:20:00 -0700

    Implement ApplicationExceptionHandler and ErrorResponse

--- src/main/java/com/practice/Sandbox/ApplicationExceptionHandler.java
+++ src/main/java/com/practice/Sandbox/ApplicationExceptionHandler.java
@@ -1,8 +1,18 @@
 package com.practice.Sandbox;
 
+import com.practice.Sandbox.exception.EntityNotFoundException;
+import com.practice.Sandbox.exception.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Arrays;
+
 public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
-  @ExceptionHandler(Exception.class)
+  @ExceptionHandler({EntityNotFoundException.class})
+  public ResponseEntity<Object> handleResourceNotFoundException(RuntimeException ex) {
+    ErrorResponse error = new ErrorResponse(Arrays.asList(ex.getMessage()));
+    return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+  }
 }
