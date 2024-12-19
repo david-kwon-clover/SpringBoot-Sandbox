f8ec7bf83342f402f8fdaa404f9e620e8f4e74aa
David Kwon <david.kwon@clover.com>
2024-12-19 14:26:11 -0700

    Update SecurityConfig

--- src/main/java/com/practice/Sandbox/security/SecurityConfig.java
+++ src/main/java/com/practice/Sandbox/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.practice.Sandbox.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,7 +18,8 @@ public class SecurityConfig {
             .frameOptions(frameOptions -> frameOptions.disable()))
         .authorizeHttpRequests((authorize) -> authorize
             .requestMatchers("/h2/**").permitAll()
-            .requestMatchers("/user/signup").permitAll());
+            .requestMatchers("/user/signup").permitAll())
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
     return http.build();
   }
