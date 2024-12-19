0a2369415d0f59fd841526741b720a400c18a9f8
David Kwon <david.kwon@clover.com>
2024-12-19 13:58:42 -0700

    Set up User entity

--- src/main/java/com/practice/Sandbox/entity/User.java
+++ src/main/java/com/practice/Sandbox/entity/User.java
@@ -1,4 +1,37 @@
 package com.practice.Sandbox.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
 public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Long id;
+
+  @NotEmpty(message = "username cannot be empty")
+  @NonNull
+  @Column(name = "username")
+  private String username;
+
+  @NotEmpty(message = "password cannot be empty")
+  @NonNull
+  @Column(name = "password")
+  private String password;
 }
