09d71c0173a76a280c400c0c090edc0bb7563ab7
David Kwon <david.kwon@clover.com>
2024-12-20 08:38:16 -0700

    Add successfulAuthentication method logic in AuthenticationFilter

--- src/main/java/com/practice/Sandbox/security/filter/AuthenticationFilter.java
+++ src/main/java/com/practice/Sandbox/security/filter/AuthenticationFilter.java
@@ -1,8 +1,12 @@
 package com.practice.Sandbox.security.filter;
 
+import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.practice.Sandbox.entity.User;
+import com.practice.Sandbox.security.SecurityConstants;
 import com.practice.Sandbox.security.manager.CustomAuthenticationManager;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
@@ -10,8 +14,10 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
+import com.auth0.jwt.JWT;
 import java.io.IOException;
+
+
 @AllArgsConstructor
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -26,5 +32,14 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     } catch (IOException e) {
       throw new RuntimeException();
     }
+  }
+
+  @Override
+  protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+    String token = JWT.create()
+        .withSubject(authResult.getName())
+        .withExpiresAt(new Date(System.currentTimeMillis() + SecurityConstants.TOKEN_EXPIRATION))
+        .sign(Algorithm.HMAC512(SecurityConstants.SECRET_KEY));
+    response.addHeader(SecurityConstants.AUTHORIZATION, SecurityConstants.BEARER + token);
   }
 }
