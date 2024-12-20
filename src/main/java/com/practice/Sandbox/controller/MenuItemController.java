141cf3f59bc415d4751e7e45475d9df5cd61f4dc
David Kwon <david.kwon@clover.com>
2024-12-20 12:09:15 -0700

    Implement CRUD methods in MenuItemController

--- src/main/java/com/practice/Sandbox/controller/MenuItemController.java
+++ src/main/java/com/practice/Sandbox/controller/MenuItemController.java
@@ -1,9 +1,20 @@
 package com.practice.Sandbox.controller;
 
+import com.practice.Sandbox.entity.MenuItem;
 import com.practice.Sandbox.service.MenuItemService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/menu")
@@ -12,13 +23,32 @@ public class MenuItemController {
 
   private MenuItemService menuItemService;
 
-  // get by id
+  @GetMapping("/{id}")
+  public ResponseEntity<MenuItem> getMenuItem(@PathVariable Long id) {
+    return new ResponseEntity<>(menuItemService.getMenuItem(id), HttpStatus.OK);
+  }
 
-  // get all
+  @GetMapping("/all")
+  public ResponseEntity<List<MenuItem>> getAllMenuItems() {
+    return new ResponseEntity<>(menuItemService.getAllMenuItems(), HttpStatus.OK);
+  }
 
   // create
+  @PostMapping
+  public ResponseEntity<MenuItem> createMenuItem(@RequestBody MenuItem menuItem) {
+    return new ResponseEntity<>(menuItemService.saveMenuItem(menuItem), HttpStatus.CREATED);
+  }
 
   // update by id
+  @PutMapping("/{id}")
+  public ResponseEntity<MenuItem> updateMenuItem(@PathVariable Long id, @RequestBody MenuItem menuItem) {
+    return new ResponseEntity<>(menuItemService.updateMenuItem(id, menuItem), HttpStatus.OK);
+  }
 
   // delete
+  @DeleteMapping("/{id}")
+  public ResponseEntity<MenuItem> deleteMenuItem(@PathVariable Long id) {
+    menuItemService.deleteMenuItem(id);
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
 }
